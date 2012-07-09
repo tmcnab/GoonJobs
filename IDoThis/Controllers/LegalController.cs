a926f442b43b86b49a415ae8cf88eb986effab98
Solidus <tristan@seditious-tech.com>
2012-07-09 18:56:08 +1000

    Added legal and twitter link

---
+++ IDoThis/Controllers/LegalController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDoThis.Controllers
+{
+    public class LegalController : Controller
+    {
+        //
+        // GET: /Legal/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
