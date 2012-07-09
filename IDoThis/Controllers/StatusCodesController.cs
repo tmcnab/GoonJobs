eeaae4dfaf44538d1e777bf7045a0e2f637c8062
Solidus <tristan@seditious-tech.com>
2012-07-09 18:14:36 +1000

    Added custom error pages

---
+++ IDoThis/Controllers/StatusCodesController.cs
@@ -0,0 +1,38 @@
+﻿namespace IDoThis.Controllers
+{
+    using System.Web.Mvc;
+    using AttributeRouting.Web.Mvc;
+
+    public class StatusCodesController : Controller
+    {
+        #region 402
+
+        [Route("402", HttpVerbs.Get)]
+        public ActionResult HttpStatus402()
+        {
+            return View("402");
+        }
+
+        #endregion
+
+        #region 404
+
+        [Route("404", HttpVerbs.Get)]
+        public ActionResult HttpStatus404()
+        {
+            return View("404");
+        }
+
+        #endregion
+
+        #region 500
+
+        [Route("500", HttpVerbs.Get)]
+        public ActionResult HttpStatus500()
+        {
+            return View("500");
+        }
+
+        #endregion
+    }
+}
